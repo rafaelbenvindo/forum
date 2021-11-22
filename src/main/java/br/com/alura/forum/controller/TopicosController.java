e86eab4f72f9546451068b7abfb31dbf173ebabb
Rafael Benvindo <rafael.benvindo@trt1.jus.br>
2021-11-22 16:19:46 -0300

    Aula 2, atividade 6: Cria TopicoDto e atualiza retorno da listagem

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -2,10 +2,12 @@ package br.com.alura.forum.controller;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 
@@ -13,11 +15,15 @@ import br.com.alura.forum.modelo.Topico;
 public class TopicosController {
 
 	@RequestMapping("/topicos")
-	public List<Topico> lista() {
-		return Arrays.asList(
+	public List<TopicoDto> lista() {
+		return converter(Arrays.asList(
 				new Topico("Duvida", "Duvida com Spring 1", new Curso("Spring", "Programacao")),
 				new Topico("Duvida", "Duvida com Spring 2", new Curso("Spring", "Programacao")),
 				new Topico("Duvida", "Duvida com Spring 3", new Curso("Spring", "Programacao"))
-				);
+				));
+	}
+
+	private List<TopicoDto> converter(List<Topico> topicos) {
+		return topicos.stream().map(TopicoDto::new).collect(Collectors.toList());
 	}
 }
